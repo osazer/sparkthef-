c8d7dc6bdafb822219f25fad3fb487d21d733430
osamzerx <97283606+osazer@users.noreply.github.com>
2025-10-24 19:39:32 +0300

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+}
+
+android {
+    namespace = "com.example.spark_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.spark_app"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+}
---
+++ android/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
+plugins {
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
+
+gradle.settingsEvaluated {
+    println("Gradle home: ${gradle.gradleUserHomeDir}")
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+    evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
